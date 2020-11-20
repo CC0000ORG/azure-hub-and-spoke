b1071a4f1f58104d16976b6e75670f332b972db0
CC0001gmxde <72203217+CC0001gmxde@users.noreply.github.com>
2020-11-20 19:01:17 +0100

    Add files via upload

--- azuredeploy.ps1
+++ azuredeploy.ps1
@@ -1,4 +1,11 @@
-#use this command when you need to create a new resource group for your deployment
-New-AzResourceGroup -Name <resource-group-name> -Location <resource-group-location>
+# azure-hub-and-spoke
+# use this command when you need to create a new resource group for your deployment
 
-New-AzResourceGroupDeployment -ResourceGroupName <resource-group-name> -TemplateUri https://raw.githubusercontent.com/CC0001MK/azure-hub-and-spoke/main/azuredeploy.json
+$resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
+$resourceGrouplocation = Read-Host -Prompt "Enter the location (i.e. centralus)"
+
+New-AzResourceGroup -Name $resourceGroupName -Location $resourceGrouplocation
+
+New-AzResourceGroupDeployment `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateUri "https://raw.githubusercontent.com/CC0001MK/azure-hub-and-spoke/main/azuredeploy.json"
